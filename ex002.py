a3cf5af7989500133364d8ccf359849fe1f0c02f
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-09 11:15:50 -0300

    ex002.py

---
+++ ex002.py
@@ -0,0 +1,9 @@
+'''
+2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+Ex:
+Qual é o seu nome? João da Silva
+Olá João da Silva, é um prazer te conhecer!
+'''
+
+nome = input('Digite seu nome: ')
+print(f'Olá, {nome}, é um prazer te conhecer!')
