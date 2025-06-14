6ae1a0a90b30b42419b203111bc370b655976b2a
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-14 14:10:22 -0300

    correcao

--- ex012.py
+++ ex012.py
@@ -6,7 +6,7 @@ PREÇO PROMOCIONAL, com 5% de desconto.
 preco_produto = input('Digite o preço do produto: ')
 float_preco_produto = float(preco_produto)
 
-valor_promocional = float_preco_produto - (5 / float_preco_produto * 100)
+valor_promocional = float_preco_produto - (float_preco_produto * 5 / 100)
 
 print(f'Valor do produto R${float_preco_produto :.2f}')
 print(f'Valor do produto com 5% de desconto: R${valor_promocional :.2f}')
