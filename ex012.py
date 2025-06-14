cf98ab1e571ff1030ee9bfd78e126ef6ab9895ff
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-14 13:59:50 -0300

    calculo de porcentagem

---
+++ ex012.py
@@ -0,0 +1,12 @@
+''' 
+Crie um programa que leia o preço de um produto, calcule e mostre o seu
+PREÇO PROMOCIONAL, com 5% de desconto. 
+'''
+
+preco_produto = input('Digite o preço do produto: ')
+float_preco_produto = float(preco_produto)
+
+valor_promocional = float_preco_produto - ((5 / 100) * 100)
+
+print(f'Valor do produto R${float_preco_produto :.2f}')
+print(f'Valor do produto com 5% de desconto: R${valor_promocional :.2f}')
