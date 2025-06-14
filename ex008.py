b44e0ec8b80ee411810a465bd59c24c254d1ce59
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-14 13:31:45 -0300

    conversor de distancias

--- ex008.py
+++ ex008.py
@@ -3,7 +3,7 @@
 relativos em outras medidas.
 Ex:
 Digite uma distância em metros: 185.72
-A distância de 85.7m corresponde a:
+A distância de 185.72m corresponde a:
 0.18572Km
 1.8572Hm
 18.572Dam
@@ -12,3 +12,21 @@ A distância de 85.7m corresponde a:
 185720.0mm
 '''
 
+distancia_metros = input('Digite uma distância em metros: ')
+float_distancia_metros = float(distancia_metros)
+
+conversor_km = float_distancia_metros / 1000
+conversor_hm = float_distancia_metros / 100
+conversor_dam = float_distancia_metros / 10
+
+conversor_dm = float_distancia_metros * 10
+conversor_cm = float_distancia_metros * 100
+conversor_mm = float_distancia_metros * 1000
+
+print(f'A distância de {float_distancia_metros:.2f}m corresponde a:')
+print(f'{conversor_km}Km')
+print(f'{conversor_hm}Hm')
+print(f'{conversor_dam}Dam')
+print(f'{conversor_dm}dm')
+print(f'{conversor_cm}cm')
+print(f'{conversor_mm}mm')
