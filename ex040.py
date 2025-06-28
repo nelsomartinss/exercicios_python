0fa1e6465f865c9f110ae861b8e79845662f304d
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-28 17:44:15 -0300

    contagem pulando de 3 em 3

---
+++ ex040.py
@@ -0,0 +1,12 @@
+'''
+Crie um aplicativo que mostre na tela a seguinte contagem:
+0 3 6 9 12 15 18 Acabou!
+'''
+
+numero = 0
+
+while numero <= 18:
+    print(numero, end=' ')
+    numero+=3
+
+print('Acabou!')
