bd1f5c8aaf6d46d99b9df8fb7ff7534a46a48566
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-28 17:41:27 -0300

    contagem decrescente do 10 ao 3

---
+++ ex039.py
@@ -0,0 +1,12 @@
+'''
+Faça um algoritmo que mostre na tela a seguinte contagem:
+10 9 8 7 6 5 4 3 Acabou!
+'''
+
+numero = 10
+
+while numero >= 3:
+    print(numero, end=' ')
+    numero-=1
+
+print('Acabou!')
