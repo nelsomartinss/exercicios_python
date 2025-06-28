99fc4c8cd1a558b9926316ae02d405c68a78ac51
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-06-28 17:40:00 -0300

    contagem do 6 ao 11

---
+++ ex038.py
@@ -0,0 +1,12 @@
+'''
+38) Escreva um programa que mostre na tela a seguinte contagem:
+6 7 8 9 10 11 Acabou!
+'''
+
+numero = 6
+
+while numero <= 11:
+    print(numero, end=' ')
+    numero+=1
+
+print('Acabou!')
