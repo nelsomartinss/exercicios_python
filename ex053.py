279e01de38cb54f199b620073a75de7f606a66f6
Nelson Martins <nelsonmartinsjp2001@gmail.com>
2025-07-05 16:04:24 -0300

    medias de idade

--- ex053.py
+++ ex053.py
@@ -10,19 +10,12 @@ e) Quantas mulheres tem mais de 20 anos
 i = 1
 qtd_sexo_feminino = 0
 qtd_sexo_masculino = 0
+soma_idades = 0
+soma_idade_homens = 0
+qtd_mulheres_mais_20 = 0
 
 while i <= 5:
-    sexo = input(F'\n[F] para FEMININO e [M] para MASCULINO\nDigite o sexo da {i}º pessoa: ').upper()
-
-    if sexo == 'F':
-        qtd_sexo_feminino+=1
-    elif sexo == 'M':
-        qtd_sexo_masculino+=1
-    else:
-        print('\nDigite apenas F ou M!')
-        continue
-
-    idade = input(f'Digite a idade dessa pessoa: ')
+    idade = input(f'\nDigite a idade da {i}º pessoa: ')
     
     try:
         idade = int(idade)
@@ -30,4 +23,28 @@ while i <= 5:
         print('Digite apenas valores númericos inteiros para idade.')
         continue
 
+    sexo = input(F'\n[F] para FEMININO e [M] para MASCULINO\nDigite o sexo dessa pessoa: ').upper()
+
+    if sexo == 'F':
+        qtd_sexo_feminino+=1
+        if idade > 20:
+            qtd_mulheres_mais_20+=1
+    elif sexo == 'M':
+        qtd_sexo_masculino+=1
+        soma_idade_homens+=idade
+    else:
+        print('\nDigite apenas F ou M!')
+        continue
+    
+    soma_idades+=idade
     i+=1
+
+media_idades = soma_idades / 5
+media_idades_homens =  soma_idade_homens / qtd_sexo_masculino
+
+print(f"\nQuantidade de homens cadastrados: {qtd_sexo_masculino}")
+print(f"Quantidade de mulheres cadastradas: {qtd_sexo_feminino}")
+print(f"A média das idades digitadas é: {media_idades}")
+print(f"A média de idade dos homens é: {media_idades_homens}")
+print(f"A quantidade de mulheres maiores de 20 anos: {qtd_mulheres_mais_20}\n")
+    
